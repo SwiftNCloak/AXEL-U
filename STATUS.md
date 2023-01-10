d57a991e7014f3ccc7600341f96a675296594dc3
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-10 21:48:37 +0800

    A-001 Progress for AXEL-U!

---
+++ STATUS.md
@@ -0,0 +1,24 @@
+# AXEL-U Research Bot Status
+
+Made by **Mark James Bonifacio**
+
+---
+
+AXEL-U is a research and development chatbot made for Developing Stuffs (and for Mark, the creator) that will be used to serve as a scientific journal and progress tracker to see which AI version would be near-sentient. 
+
+Language of AXEL-U **will ONLY BE in English**.
+
+---
+
+> `Probably one of the most satisfying things I've developed.`
+
+---
+
+# Progress
+
+> `Version A - Started in [JAN 10, 2023 - 9:00PM]`
+
+|A|Status|Features
+|--------|--------|--------|
+|    001    |    Working    |   - Chatbot actually works and speaks like a normal human, thanks to data given.
+| 002 | Coming Soon | N/A
---
+++ main.py
@@ -0,0 +1,77 @@
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+import nltk
+import numpy as np
+import random
+import string 
+
+# Don't forget to comment each line in order to not forget everything.
+
+f = open('chatbot.txt','r',errors = 'ignore')
+
+raw=f.read()
+raw=raw.lower()
+
+verLetter = 'A-'
+verNum = '001'
+
+# nltk.download('punkt') # first time
+# nltk.download('wordnet') # first time
+# nltk.download('omw-1.4') # first time
+
+sent_tokens = nltk.sent_tokenize(raw)
+word_tokens = nltk.word_tokenize(raw)
+
+lemmer = nltk.stem.WordNetLemmatizer()
+
+def LemTokens(tokens):
+    return [lemmer.lemmatize(token) for token in tokens]
+remove_punct_dict = dict((ord(punct), None) for punct in string.punctuation)
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
+
+GREETING_INPUTS = ("hello", "hi", "greetings", "sup", "what's up","hey", "hey there")
+GREETING_RESPONSES = ["hi", "hey", "hi there", "hello", "hey there", "well, hello there"]
+def greeting(sentence):
+ 
+    for word in sentence.split():
+        if word.lower() in GREETING_INPUTS:
+            return random.choice(GREETING_RESPONSES)
+
+def response(user_response):
+    robo_response=''
+    sent_tokens.append(user_response)
+    TfidfVec = TfidfVectorizer(tokenizer=LemNormalize, stop_words='english')
+    tfidf = TfidfVec.fit_transform(sent_tokens)
+    vals = cosine_similarity(tfidf[-1], tfidf)
+    idx=vals.argsort()[0][-2]
+    flat = vals.flatten()
+    flat.sort()
+    req_tfidf = flat[-2]
+    if(req_tfidf==0):
+        robo_response=robo_response+"I apologize... I do not understand what you said. Could you repeat that for me?"
+        return robo_response
+    else:
+        robo_response = robo_response+sent_tokens[idx]
+        return robo_response
+
+flag=True
+print("AXEL-U: My name is " + verLetter + verNum + ". Type 'bye' or 'end' for the chat to stop. I am still not as advanced as what I am supposed to be, but I'll be learning to improve myself more..")
+while(flag==True):
+    user_response = input()
+    user_response=user_response.lower()
+    if(user_response!='bye' or user_response!='end'):
+        if(user_response=='thanks' or user_response=='thank you' or user_response=='thank u'):
+            flag=False
+            print("AXEL-U: No problem.")
+        else:
+            if(greeting(user_response)!=None):
+                print("AXEL-U: "+greeting(user_response))
+            else:
+                print("AXEL-U: ",end="")
+                print(response(user_response))
+                sent_tokens.remove(user_response)
+    else:
+        flag=False
+        print("AXEL-U: Bye! Thank you for talking!")
