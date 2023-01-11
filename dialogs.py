e78497ef6963dcb0f010a615a4ea316c1b6c69d5
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-11 11:27:24 +0800

    Modified both of the codes

--- dialogs.py
+++ dialogs.py
@@ -8,5 +8,5 @@ while True:
 
 with open('C:/Users/Mark James/Desktop/COMSCI LABS/artificial-intelligence-lab/chatbot-1/AXEL-U/chatbot.txt', 'a') as f:
     for response in responses:
-        f.write('\n\n' + response)
+        f.write('\n' + response)
 print("AXEL-U dialogs are now placed in chatbot.txt")
--- discord-gather.py
+++ discord-gather.py
@@ -9,6 +9,6 @@ async def on_message(message):
     if message.author == client.user:
         return
     with open('C:/Users/Mark James/Desktop/COMSCI LABS/artificial-intelligence-lab/chatbot-1/AXEL-U/chatbot.txt', 'a') as f:
-        f.write(f'\n\n{message.content}')
+        f.write(f'\n{message.content}')
 
 client.run('MTA2MjU2Mjk4NjE1MTEzNzMwMQ.GPwhe8.Dfyr6FsEO4gfZs-sQNfVS05jMHTEYDNG63mFyA')
