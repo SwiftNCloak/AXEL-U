66d232f046bdb9d391b6dc585b9ab8a6cb9b871d
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-12 12:05:41 +0800

    Discord bot client and chatbot corpus updated

--- discord-gather.py
+++ discord-gather.py
@@ -11,4 +11,17 @@ async def on_message(message):
     with open('C:/Users/Mark James/Desktop/COMSCI LABS/artificial-intelligence-lab/chatbot-1/AXEL-U/chatbot.txt', 'a') as f:
         f.write(f'\n{message.content}')
 
+@client.event
+async def on_ready():
+    print('AXEL-U is now activated')
+
+    members = 0
+    for guild in client.guilds:
+        members += guild.member_count - 1
+
+    await client.change_presence(activity = discord.Activity(
+        type = discord.ActivityType.watching,
+        name = f'{members} members'
+    ))
+
 client.run('MTA2MjU2Mjk4NjE1MTEzNzMwMQ.GPwhe8.Dfyr6FsEO4gfZs-sQNfVS05jMHTEYDNG63mFyA')
