dbb6f201441dfb5eabcf793abfa08a3c8d49a05f
SwiftNCloak <markyjamesbonifacio@gmail.com>
2023-01-16 15:01:39 +0800

    Train experiment somehow working

--- train.py
+++ train.py
@@ -1,26 +1,27 @@
 import nltk
 from nltk.util import ngrams
+from nltk import word_tokenize
 from nltk.lm import MLE
+from nltk.lm.preprocessing import pad_both_ends, padded_everygram_pipeline
 
-# Initialize an empty list to store the user inputs
-user_inputs = []
+with open('C:/Users/Mark James/Desktop/COMSCI LABS/artificial-intelligence-lab/chatbot-1/AXEL-U/chatbot.txt', "r") as file:
+    contents = file.read()
 
-# Chatbot loop
-while True:
-    # Get user input
-    user_input = input("You: ")
-    if user_input == 'exit':
-        break
-    # Append user input to the list
-    user_inputs.append(user_input)
-    # Tokenize the user inputs
-    tokens = nltk.word_tokenize(" ".join(user_inputs))
-    # Create a list of trigrams
-    trigrams = list(ngrams(tokens, 3))
-    # Create a language model using Maximum Likelihood Estimation
-    lm = MLE(3)
-    # Train the language model on the trigrams
-    lm.fit(trigrams)
-    # Use the trained model to generate a response
-    response = lm.generate(30)
-    print("Chatbot: " + " ".join(response))
+paddedLine = [list(pad_both_ends(word_tokenize(contents), n=2))]
+
+train, vocab = padded_everygram_pipeline(2, paddedLine)
+
+lm = MLE(2)
+
+clean_text = contents.replace('<s>','').replace('</s>','')
+token =  ['<s>'] + nltk.word_tokenize(clean_text) + ['</s>'] + nltk.word_tokenize(clean_text)
+lm.fit(train, vocab)
+
+generated_sentence = lm.generate(20)
+while len(generated_sentence) <= 20:
+    generated_sentence = lm.generate(20)
+
+with open('C:/Users/Mark James/Desktop/COMSCI LABS/artificial-intelligence-lab/chatbot-1/AXEL-U/train-data.txt', 'a') as f:
+        f.write(f'\n{generated_sentence}')
+
+print(" ".join(generated_sentence))
